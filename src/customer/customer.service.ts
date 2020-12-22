7ebd72805721b30ff77ec95d0c0bf8bea8f7fee4
Carlos <c.morenomontesdeoca@gmail.com>
2020-12-22 22:42:42 +0100

    module and service

---
+++ src/customer/customer.service.ts
@@ -0,0 +1,28 @@
+// ./src/customer/customer.service.ts 
+
+import { Injectable } from '@nestjs/common';
+
+import { Model } from 'mongoose';
+
+import { InjectModel } from '@nestjs/mongoose'
+
+import { Customer } from './interfaces/customer.interface';
+
+import { CreateCustomerDTO } from './dto/create-customer.dto';
+
+
+@Injectable()
+export class CustomerService {
+
+//TODO: constructor
+
+//TODO: GetAll
+
+//TODO: GetOne
+
+//TODO: Update
+
+//TODO: Delete
+
+}
+
