9ed222780f97354047ce3fae97315e50784e9bdc
Carlos <c.morenomontesdeoca@gmail.com>
2020-12-22 22:11:19 +0100

    sintax fix

--- src/customer/dto/customer.dto.ts
+++ src/customer/dto/customer.dto.ts
@@ -1,10 +1,10 @@
 // ./src/customer/dto/create-customer.dto.ts 
 
 export class CreateCustomerDTO {
-    readonly DNI: String,
-    readonly nombre: String,
-    readonly apellidos: String,
-    readonly mail: String,
-    readonly teléfono: String,
-    readonly creado_el: Date
+    readonly DNI: String;
+    readonly nombre: String;
+    readonly apellidos: String;
+    readonly mail: String;
+    readonly teléfono: String;
+    readonly creado_el: Date;
 }
