d49d111b3540fb1b19ca8ca52b4b0a5357031254
Carlos <c.morenomontesdeoca@gmail.com>
2020-12-24 22:44:48 +0100

    controller

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -8,13 +8,32 @@ import { CreateCustomerDTO } from './dto/create-customer.dto';
 export class CustomerController {
     constructor(private customerService: CustomerService) { }
 
-    // add a customer
+    // Agregar Afiliado
     @Post('/create')
 
     async addCustomer(@Res() res, @Body() CreateCustomerDTO: CreateCustomerDTO) {
         const customer = await this.customerService.addCustomer(CreateCustomerDTO);
         return res.status(HttpStatus.OK).json({
-            message: "El afiliado se ha creado exitosamente!"
+            message: "El afiliado se ha creado exitosamente!",
+            customer
         })
     }
+
+    // Mostrar lista de Afiliados
+
+    @Get('Customers')
+    async getAllCustomer(@Res() res) {
+        const customers = await this.customerService.getAllCustomer();
+        return res.status(HttpStatus.OK).json(customers);
+    }
+
+
+    // Mostrar un afiliado en particular utilizando el ID
+    @Get('customer/:customerID')
+    async getCustomer(@Res() res, @Param('CustomerID') customerID) {
+        const customer = await.this.customerService.getCustomer(customerID);
+        if (!customer) throw new NotFoundException('Afiliado no existe!')
+        return res.status(HttpStatus.OK).json(customer);
+    }
+
 }
